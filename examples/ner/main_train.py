54e9a1ae7c20215e0f85f83ea1fe4b964478c082
Jiaqiang Ruan <jiaqiangruan@jiaqiangs-air.myfiosgateway.com>
2020-09-24 09:43:08 -0400

    add glove

--- examples/ner/main_train.py
+++ examples/ner/main_train.py
@@ -12,6 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import sys
+sys.path.append("/Users/jiaqiangruan/CMU/Capstone")
+sys.path.append("/Users/jiaqiangruan/CMU/Capstone/forte")
+print(sys.path)
+
 import logging
 
 import yaml
