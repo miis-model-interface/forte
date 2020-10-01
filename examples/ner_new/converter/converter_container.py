71973f6408a277230b7a4a21f7eb0c8740498a77
YanwenLin <yanwenl@andrew.cmu.edu>
2020-10-01 01:24:58 -0400

    add basic logic for core funcs in converter_container/task_manager; add prototype for train_pipeline;

--- examples/ner_new/converter/converter_container.py
+++ examples/ner_new/converter/converter_container.py
@@ -11,10 +11,45 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+from typing import Dict
 
-class ConverterContainter():
-    def __init__(self):
+
+class ConverterContainer():
+    def __init__(self, converter_request: Dict):
+        # converter_map format:
+        # {
+        #    "Token": {
+        #        "ner": [DenseConverter],
+        #        "text": [DenseConverter, CharConverter]
+        #    }
+        # }
+        # TODO
+        self.converter_map: Dict = {}
         pass
 
-    def consume(self, instance):
-        pass
+    def consume(self, instance: Dict) -> Dict:
+        # Consume should output tensor container, the format is:
+        # {
+        #    "Token": {
+        #        "ner": {"dense": [<tensor>]},
+        #        "text": {"dense": [<tensor>], "char": [<tensor>]},
+        #    }
+        # }
+        tensor_container = {}
+
+        for annotation_name in self.converter_map.keys():
+            annotation = instance[annotation_name]
+            tensor_container[annotation_name] = {}
+
+            for label_name in self.converter_map[annotation_name].keys():
+                # Now we focus on a single label from a certain annotation
+                tensor_container[annotation_name][label_name] = {}
+
+                label = annotation[label_name]
+
+                for converter in \
+                        self.converter_map[annotation_name][label_name]:
+                    tensor_container[annotation_name][label_name][
+                        converter.name] = converter.convert(label)
+
+        return tensor_container
--- examples/ner_new/task/task_manager.py
+++ examples/ner_new/task/task_manager.py
@@ -11,10 +11,47 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+from typing import Dict, Tuple
+
+from examples.ner_new.converter.converter_container import ConverterContainer
+from forte.processors.base.base_processor import BaseProcessor
+from forte.evaluation.base.base_evaluator import Evaluator
+from forte.trainer.base.base_trainer import BaseTrainer
+from examples.ner_new.train_pipeline import TrainPipeline
+
 
 class TaskManager():
     def __init__(self):
         pass
 
-    def build_train_pipeline(self, request):
+    def build_train_pipeline(self, request: Dict) -> TrainPipeline:
+        # TODO: add check
+
+        # Search trainer, predictor, evaluator.
+        trainer, evaluator, predictor = self.search_model(request['model_type'])
+
+        # Config trainer predictor, evaluator.
+        # TODO
+
+        # Build data pack request.
+        datapack_request = self.generate_datapack_request(request)
+
+        # Build converters.
+        converter_container_p2t, converter_container_t2p = \
+            self.generate_converters(request)
+
+        # Build pipeline.
+        train_pipe = TrainPipeline()
+
+        return train_pipe
+
+    def search_model(self, type: str) \
+            -> Tuple[BaseTrainer, Evaluator, BaseProcessor]:
+        pass
+
+    def generate_datapack_request(self, request: Dict) -> Dict:
+        pass
+
+    def generate_converters(self, request: Dict) \
+        -> Tuple[ConverterContainer, ConverterContainer]:
         pass
